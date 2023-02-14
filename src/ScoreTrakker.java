0f553f40327831cbd81d7b8ab4277447b57cbf9f
Samuel Colombo <sam@softwarfare.com>
2023-02-13 19:21:33 -0700

    Working Final -> main

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
+
 public class ScoreTrakker {
 	private ArrayList<Student> students;
 	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
@@ -19,24 +21,6 @@ public class ScoreTrakker {
 	}
 	
 	/**
-	 * isNumber: helper method to check if passed in string is a number.
-	 * @param inString: the input string need to be check.
-	 * @return false, if not an integer, and true, if it is not.
-	 */
-	public static boolean isNumber(String inString) {
-		if(inString == null) {
-			return false;
-		}
-		try {
-			// parse if the string is a number, if not throw exception of NumberFormat
-			int parseInt = Integer.parseInt(inString);
-		} catch (NumberFormatException e){
-			return false;
-		}
-		return true;
-	}
-	
-	/**
 	 * loadDataFile: open the file and read its content and add to array list of student object.
 	 * @param fileName: name of target file, if exist.
 	 * @throws Exception: if the read value that was supposed to score is not a number, throws an exception.
@@ -44,61 +28,57 @@ public class ScoreTrakker {
 	public void loadDataFile (String fileName) throws Exception{
 		// read the file
         FileReader read = null;
+        
+        //try catch opening file
 		try {
+			// read & scan the file
 			read = new FileReader(fileName);
-		} catch (FileNotFoundException e1) {
-			System.out.println("Can't open file: " + fileName);
-		}
-        // scan the file
-        Scanner scan = new Scanner(fileName);
-        // create temp student to be add to the array list
-        Student curStudent = new Student();
-        // create a temp string to catch what is read
-        String curLine = "";
-        // char to keep track of what being read in, N for name, S for score
-        char curRead = 'N';
-        // loop through the scanned file to set name and score then add to the array list
-        while(scan.hasNextLine()) {
-        	curLine = scan.nextLine();
-        	// check if read value is a number for when reading a score
-        	if (isNumber(curLine) && curRead == 'S') {
-        		// set score
-                curStudent.setScore(Integer.parseInt(curLine));
-                // add student to array list
-                this.students.add(curStudent);
-                // change for the next read to be name again
-                curRead = 'N';
-        	} else {
-        		if(curRead == 'N') {
-        			// set name
-        			curStudent.setName(curLine);
-            		// change for the next read
-            		curRead = 'S';
-        		} else {
-        			// if the current reading is not score and not a number
-        			throw new Exception("Incorrect format, not a valid score: " + curLine);
-        		}
-        	}
-        }
-        // close read and scan
-        scan.close();
-        try {
-			read.close();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			Scanner scan = new Scanner(read);
+        
+	        // loop through the scanned file to set name and score then add to the array list
+	        while(scan.hasNextLine()) {
+	        	String name = scan.nextLine();
+	        	String s_score = scan.nextLine();
+	        	try {
+	        		int score = Integer.parseInt(s_score);
+	        		Student newStudent = new Student(name, score);
+	        		students.add(newStudent);
+	        	} catch (NumberFormatException a) { //catch non-int score
+	        		System.out.println("Incorrect format for " + name + " not a valid score: " + s_score + "\n");
+	        	}
+	        }
+	        
+	        // try catch close read and scan
+	        try { read.close(); }
+			catch (IOException e) { e.printStackTrace(); }
+			
+		} catch (FileNotFoundException exep) { //catch FileNotFoundException
+			throw exep;
 		}
 	}
 	public void printInOrder () {
+		System.out.println("Student Score List");
 		Collections.sort(this.students);
-		Iterator<Student> iter = this.students.iterator();
+		Iterator<Student> iter = this.students.iterator(); //iterator for loop
 		while (iter.hasNext()) {
 			System.out.println(iter.next());
 		}
+		System.out.println('\n'); //newline after printing all students
 	}
 	public void processFiles() throws Exception {
-		loadDataFile("scores.txt");
-		printInOrder();
+		for (String file: this.files) {
+			try { //try catch for file not founds from loadDataFile
+				ScoreTrakker st = new ScoreTrakker();
+				st.loadDataFile(file);
+				st.printInOrder();
+			} catch (FileNotFoundException a) { 
+				System.out.println("Can't open file: " + file); //print error message
+			}
+		}
 	}
+	/*
+	 * Main, runs processFiles
+	 */
 	public static void main(String[] args) throws Exception {
 		ScoreTrakker trakker = new ScoreTrakker();
 		trakker.processFiles();
--- src/Student.java
+++ src/Student.java
@@ -4,17 +4,32 @@ public class Student implements Comparable<Student>{
 	private String name;
 	private int score;
 
+	public Student (String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+	
+	/*
+	 * Override compareTo for Collections.sort
+	 */
 	@Override
 	public int compareTo(Student s) {
 		return name.compareTo(s.name);
 	}
 	
+	/*
+	 * Override toString for printing student behavior
+	 */
 	@Override
 	public String toString() {
 		return name + " " + score;
 	}
 	
-	// getters and setters
+	
+	/*
+	 * Getters & Setters:
+	 */
 	public String getName() {
 		return name;
 	}
